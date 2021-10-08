275e8858d86031b9905440dbcf0488236da1533a
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-08 22:04:02 +0200

    version8
    fonction creauser

--- projet1/script1github.ps1
+++ projet1/script1github.ps1
@@ -1,38 +1,63 @@
-﻿#cmdcrea5user 
-#pble sur la boucle foreach if else, utilisateur creer fichier CSV
-#revoir les conditions si l'utilisateur existe déjà 
-#prevoir de mettre le bon user dans le bon UO
+﻿<#revoir les conditions si l'utilisateur existe déjà 
+prevoir de mettre le bon user dans le bon UO
+integrer les users dans les groupes
+mot de passe dans le fichier ou defini par defaut avec chgt obligatoire
+groupe critique a introduire
 
+#>
 Clear-Host
 
- # importation du fichier CSV
+# importation du fichier CSV
+# import OK
 
  $CSVData = Import-CSV -Path "C:\Scripts\AD_USERS\Utilisateurs.csv" -Delimiter ";" -Encoding UTF8
 Write-Host "Fichier Importé"
 
- #nombre d'utilisateur dans la liste
+#verification du nombre d'utilisateur dans la liste
+#a effacer plus tard
 " il y a {0} utilisateurs dans le tableau" -F ($CSVData.count)
 
 # fonction de test et creation user
 
-function CreationUser {
+function CreaUser {
 
   #lister les variables
-      param (
-          [string] $UtilisateurPrenom = $Utilisateur.Prenom ,
-          [string] $UtilisateurNom = $Utilisateur.Nom ,
-          [string] $UtilisateurLogin = ($UtilisateurPrenom).Substring(0,1) + $UtilisateurNom ,
-          [string] $UtilisateurEmail = "$UtilisateurLogin@acme.fr" ,
-          [string] $UtilisateurMotDePasse = "Ricoh80700" ,
-          [string] $UtilisateurFonction = $Utilisateur.Fonction )
-          
+  param (
+      [string] $UtilisateurPrenom = $Utilisateur.Prenom ,
+      [string] $UtilisateurNom = $Utilisateur.Nom ,
+      [string] $UtilisateurLogin = ($UtilisateurPrenom).Substring(0, 1) + $UtilisateurNom ,
+      [string] $UtilisateurEmail = "$UtilisateurLogin@acme.fr" ,
+      [string] $UtilisateurMotDePasse = "Ricoh80700" ,
+      [string] $UtilisateurFonction = $Utilisateur.Fonction ,
+      [string] $UtilisateurOU = $Utilisateur.Departement
+      # a completer pour faire une creation plus fine
+        )
+      
+  # creation de l'utilisateur, coeur de la foncion
+  New-ADUser -Name $UtilisateurNom  -DisplayName $UtilisateurNom -GivenName $UtilisateurPrenom -Surname $UtilisateurNom -SamAccountName $UtilisateurLogin -UserPrincipalName "$UtilisateurLogin@acme.fr" -EmailAddress $UtilisateurEmail -Path "OU=Stagiaires,OU=Services,DC=ACME,DC=FR" -AccountPassword(ConvertTo-SecureString -AsPlainText Ricoh80700 -Force) -PasswordNeverExpires $true -ChangePasswordAtLogon $false -CannotChangePassword $false -Enable $true
+  Write-Host "verif creation utilisateur $UtilisateurLogin"
+  #ecriture de la creation de l'utlisateur
+  Write-Output "Creation de l'utilisateur : $UtilisateurLogin ($UtilisateurNom $UtilisateurPrenom)"
+
+  #validé la création d'un utilisateur $samaccountname =$UtilisateurLogin
+  Get-ADUser -Identity $UtilisateurLogin
+  # faire apres le premier test l'insertion du service et de l' OU desiré
+
+      
+                }
  
           Foreach($Utilisateur in $CSVData)
           {
               "{0} est un utilisateur" -F $Utilisateur
+              $UtilisateurPrenom = $Utilisateur.Prenom
+              $UtilisateurNom = $Utilisateur.Nom
+              $UtilisateurLogin = ($UtilisateurPrenom).Substring(0,1) + $UtilisateurNom
+              $UtilisateurEmail = "$UtilisateurLogin@acme.fr"
+              $UtilisateurMotDePasse = "Ricoh80700"
+              $UtilisateurFonction = $Utilisateur.Fonction
                   
                 # Vérifier la présence de l'utilisateur dans l'AD
-          
+                # fonction de test utilisateur
           if (Get-ADUser -Filter {SamAccountName -eq $UtilisateurLogin})
           {
              Write-Warning "L'identifiant $UtilisateurLogin existe déjà dans l'AD"
@@ -43,15 +68,9 @@ function CreationUser {
           else 
           # creation de l'utilisateur
           {
-          New-ADUser -Name $UtilisateurNom  -DisplayName $UtilisateurNom -GivenName $UtilisateurPrenom -Surname $UtilisateurNom -SamAccountName $UtilisateurLogin -UserPrincipalName "$UtilisateurLogin@acme.fr" -EmailAddress $UtilisateurEmail -Path "OU=Stagiaires,OU=Services,DC=ACME,DC=FR" -AccountPassword(ConvertTo-SecureString -AsPlainText Ricoh80700 -Force) -PasswordNeverExpires $true -ChangePasswordAtLogon $false -CannotChangePassword $false -Enable $true
-          Write-Host "verif crea user"
-          #ecriture de la creation de l'utlisateur
-          Write-Output "Creation de l'utilisateur : $UtilisateurLogin ($UtilisateurNom $UtilisateurPrenom)"
-          
-          #validé la création d'un utilisateur $samaccountname =$UtilisateurLogin
-          Get-ADUser -Identity $UtilisateurLogin
+            Write-Host " fonction CreaUser"
+            CreaUser
           
           }
           }
-          }
-          CreationUser
+          
