5b1e9f6e1e3de5134bca340953270031c57b2790
Laurent Reiffsteck <reiffsteck@gmail.com>
2021-10-08 22:13:20 +0200

    verison15
    modif variable generale
    user manuel
    modif creauser

--- projet1/script1github.ps1
+++ projet1/script1github.ps1
@@ -5,12 +5,21 @@ mot de passe dans le fichier ou defini par defaut avec chgt obligatoire
 groupe critique a introduire
 
 creation du bon format de login et nom user
+
 #>
 param ( 
-    [string ]$n =' '   
+    [string ]$n = ' ' ,  
+    [string] $UtilisateurPrenom ,
+    [string] $UtilisateurNom  ,
+    [string] $UtilisateurLogin ,
+    [string] $UtilisateurEmail = "$UtilisateurLogin@acme.fr" ,
+    [string] $UtilisateurMotDePasse = "Ricoh80700" ,
+    [string] $UtilisateurFonction ,
+    [string] $UtilisateurOU 
       )
-
+                 
 Clear-Host
+
 
 #declaration de la fonction Creauser
 
@@ -29,8 +38,21 @@ function CreaUser {
           )
         
     # creation de l'utilisateur, coeur de la foncion
-    New-ADUser -Name $UtilisateurLogin  -DisplayName $UtilisateurLogin -GivenName $UtilisateurPrenom -Surname $UtilisateurPrenom -SamAccountName $UtilisateurLogin -UserPrincipalName "$UtilisateurLogin@acme.fr" -EmailAddress $UtilisateurEmail -Path "OU=Stagiaires,OU=Services,DC=ACME,DC=FR" -AccountPassword(ConvertTo-SecureString -AsPlainText Ricoh80700 -Force) -PasswordNeverExpires $true -ChangePasswordAtLogon $false -CannotChangePassword $false -Enable $true
-    Write-Host "verif creation utilisateur $UtilisateurLogin"
+    New-ADUser -Name $UtilisateurLogin `
+               -DisplayName $UtilisateurLogin `
+               -GivenName $UtilisateurPrenom `
+               -Surname $UtilisateurNom `
+               -SamAccountName $UtilisateurLogin `
+               -UserPrincipalName "$UtilisateurLogin@acme.fr" `
+               -EmailAddress $UtilisateurEmail `
+               -Path "OU=Stagiaires,OU=Services,DC=ACME,DC=FR" `
+               -AccountPassword(ConvertTo-SecureString -AsPlainText Ricoh80700 -Force) `
+               -PasswordNeverExpires $true `
+               -ChangePasswordAtLogon $false `
+               -CannotChangePassword $false `
+               -Enable $true
+    #-Path "OU=$UtilisateurOU,OU=Services,DC=ACME,DC=FR"
+
     #ecriture de la creation de l'utlisateur
     Write-Output "Creation de l'utilisateur : $UtilisateurLogin ($UtilisateurNom $UtilisateurPrenom)"
 
@@ -50,8 +72,8 @@ function CreaUser {
     Write-Host " fonction CreaUser"
       
                   }
-# declaration de la fonction 2 exituser
 
+# declaration de la fonction 2 exituser
 function ExistUser {
 
     #lister les variables
@@ -65,83 +87,114 @@ function ExistUser {
         [string] $UtilisateurOU = $Utilisateur.Departement
         # a completer pour faire une creation plus fine
           )
-          Write-Host " l'uilisateur $UtilisateurLogin $UtilisateurEmail existe deja"
-          Write-Host "fonction ExistUser"
+                 Write-Host " l'uilisateur $UtilisateurLogin $UtilisateurEmail existe deja dans L'AD"
+                 Write-Host "fonction ExistUser"
                     }   
-                    
+
+
 # importation du fichier CSV
 #param ($chemin= ' ')
 
-
 function Get-info () {
-  param (
-          [string] $n
-        )
-        Write-Host $n
-if ($n -eq ' ' )
-# if ($n -eq $null )
-{
-# Read-Host "vueillez donner des infos :" $chemin
-write-host "le chemin d'importation est connu  C:\Scripts\AD_USERS\Utilisateurs.csv"
-$CSVData = Import-CSV -Path "C:\Scripts\AD_USERS\Utilisateurs.csv" -Delimiter ";" -Encoding UTF8 
-Write-Host "Fichier Importé"
-" il y a {0} utilisateurs dans le tableau" -F ($CSVData.count)
+                            param (
+                                    [string] $n 
+                                   # [string] $UtilisateurPrenom ,
+                                   # [string] $UtilisateurNom  ,
+                                   # [string] $UtilisateurLogin  ,
+                                   # [string] $UtilisateurEmail = "$UtilisateurLogin@acme.fr" ,
+                                    # [string] $UtilisateurMotDePasse = "Ricoh80700" ,
+                                   # [string] $UtilisateurFonction ,
+                                    # [string] $UtilisateurOU 
+                                  )
+                                  Write-Host $n
+       if ($n -eq ' ' )
+     
+          {
+          # Read-Host "vueillez donner des infos :" $chemin
+          write-host "le chemin d'importation est connu  C:\Scripts\AD_USERS\Utilisateurs.csv"
+          # création de la variable chemin du fichier CSV 
+          # à voir avec un Read-host pour en faire une varialbe d'appel fonction
+          $CSVFile = "C:\Scripts\AD_USERS\Utilisateurs.csv"
+          $CSVData = Import-CSV -Path $CSVFile -Delimiter ";" -Encoding UTF8 
+          Write-Host "Fichier Importé"
+          " il y a {0} utilisateurs dans le tableau" -F ($CSVData.count)
 
-# condition foreach à faire car le fichier n'est pas indiqué dans la ligne de commande en terminal powershell
-Foreach ($Utilisateur in $CSVData)
-{
-"{0} est un utilisateur" -F $Utilisateur
-$UtilisateurPrenom = $Utilisateur.Prenom
-$UtilisateurNom = $Utilisateur.Nom
-$UtilisateurLogin = ($UtilisateurPrenom).Substring(0,1) + $UtilisateurNom
-$UtilisateurEmail = "$UtilisateurLogin@acme.fr"
-$UtilisateurMotDePasse = "Ricoh80700"
-$UtilisateurFonction = $Utilisateur.Fonction
-$UtilisateurOU = $Utilisateur.Departement  
-# Vérifier la présence de l'utilisateur dans l'AD
-# fonction de test utilisateur
+          # condition foreach à faire car le fichier n'est pas indiqué dans la ligne de commande en terminal powershell
 
+          Foreach ($Utilisateur in $CSVData)
+               {
+                    "{0} est un utilisateur" -F $Utilisateur
+                    $UtilisateurPrenom = $Utilisateur.Prenom
+                    $UtilisateurNom = $Utilisateur.Nom
+                    $UtilisateurLogin = ($UtilisateurPrenom).Substring(0,1) + $UtilisateurNom
+                    $UtilisateurEmail = "$UtilisateurLogin@acme.fr"
+                    $UtilisateurMotDePasse = "Ricoh80700"
+                    $UtilisateurFonction = $Utilisateur.Fonction
+                    $UtilisateurOU = $Utilisateur.Departement  
+                    # Vérifier la présence de l'utilisateur dans l'AD
+                    # fonction de test utilisateur
 
-# appel function ExistUser
-if (Get-ADUser -Filter { SamAccountName -eq $UtilisateurLogin }) 
-{
-  ExistUser
+                    
+                    # appel function ExistUser
+                    if (Get-ADUser -Filter { SamAccountName -eq $UtilisateurLogin }) 
+                        {
+                            ExistUser
 
-}
-else 
-#appel fonction CreaUser
-{ 
+                        }
+                    else 
+                    #appel fonction CreaUser
+                        { 
+                       
+                        CreaUser
 
-CreaUser
-
-}
-}
-
-
-}
-
-else 
-
-{  
-#FINIR LA FONCTION DE CREATION USER MANUEL
-# introduire à la fin la fonction creauser
-write-host " faire les questions pour la creation users"
-$UtilisateurNom = Read-Host "quel est le nom de l'utilisateur"
-write-ouput "votre est nom est " $UtilisateurNom 
-$UtilisateurPrenom =Read-Host " quel est le premnom" 
-write-output " le prenom est " $UtilisateurPrenom
-$UtilisateurFonction = Read-host "quel est la fonction, choix multiple"
-write-output "la fonction de l'utilsateur est " $UtilisateurFonction
-# voir pour OU apres les tests
-
-CreaUser
-}
-
-}
+                        }
+               }
 
 
+          }
 
-Get-info -n $n
+       else 
+       
+       {  
+           #FINIR LA FONCTION DE CREATION USER MANUEL
+       # introduire à la fin la fonction creauser
+        write-host " Vous avez choisi de créerun utilsateurs manuellement "
+        Write-host "Voic les questions pour la creation de l'utilisateur"
+        $UtilisateurNom =Read-Host "quel est le nom de l'utilisateur"
+        Write-Output "Le nom est " $UtilisateurNom 
+        $UtilisateurPrenom =Read-Host " quel est le prenom" 
+        Write-Output " le prenom est " $UtilisateurPrenom
+       # $UtilisateurLogin =Read-Host " quel est le login de l'utilisateur"
+        Write-Output "le login est" $UtilisateurLogin
+        $UtilisateurFonction =Read-host "quel est la fonction, choix multiple"
+        Write-Output "la fonction de l'utilsateur est " $UtilisateurFonction
+        $UtilisateurEmail =Read-Host " quel est l'adresse mail de l'utilisateur"
+       # voir pour OU apres les tests
+
+      # CreaUser
+      New-ADUser -Name $UtilisateurLogin `
+      -DisplayName $UtilisateurLogin `
+      -GivenName $UtilisateurPrenom `
+      -Surname $UtilisateurNom `
+      -SamAccountName $UtilisateurLogin `
+      -UserPrincipalName "$UtilisateurLogin@acme.fr" `
+      -EmailAddress $UtilisateurEmail `
+      -Path "OU=Stagiaires,OU=Services,DC=ACME,DC=FR" `
+      -AccountPassword(ConvertTo-SecureString -AsPlainText Ricoh80700 -Force) `
+      -PasswordNeverExpires $true `
+      -ChangePasswordAtLogon $false `
+      -CannotChangePassword $false `
+      -Enable $true
+      
+
+
+       }
+
+                     }
+
+     
+  
+   Get-info -n $n
 
 
 #verification du nombre d'utilisateur dans la liste
@@ -149,3 +202,8 @@ Get-info -n $n
 " il y a {0} utilisateurs dans le tableau" -F ($CSVData.count)
 
 # fonction de test et creation user
+
+
+                    
+
+
